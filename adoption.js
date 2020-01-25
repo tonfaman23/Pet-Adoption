1c075dcbf47f17278409267ec07f1c4452a1ee64
Will Baker <1will.baker@gmail.com>
2020-01-25 09:57:48 -0600

    several adjustments made, streamlined

--- adoption.js
+++ adoption.js
@@ -1,21 +1,9 @@
-const pet = [
-    {type: 'Cat', name: 'Lootsie', specialSkill: 'He finds cool things!', color: 'Orange', img: 'https://www.catster.com/wp-content/uploads/2018/01/Orange-tabby-cat-sleeping-with-eyes-closed.jpg'}
-];
-
-const pet2 = [
-    {type: 'Dino', name: 'Feathers', specialSkill: 'He does a great job keeping bad neighbors away', color: 'Brown', img: 'http://www.scified.com/u/T-Rex_33.png'}
-];
-
-const pet3 = [
-    {type: 'Dog', name: 'Oceane', specialSkill: 'She makes great coffee', color: 'Black', img: 'https://i.ytimg.com/vi/ulYd7bMRTu4/maxresdefault.jpg'}
-];
-
-const pet4 = [
-    {type: 'Cat', name: 'Mona', specialSkill: 'He hides like an expert', color: 'Black', img: 'https://images.unsplash.com/photo-1570018144466-5a3251ecd588?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'}
-];
-
-const pet5 = [
-    {type: 'Dog', name: 'Koro', specialSkill:'He protects like a champ', color: 'Gray', img: 'https://i.pinimg.com/originals/28/8b/52/288b527eb15362865e5e8f3bb9a6862b.jpg'}
+const pets = [
+    {id: 'pet-card', type: 'Cat', name: 'Lootsie', specialSkill: 'He finds cool things! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget ipsum felis. In hac habitasse platea dictumst. Maecenas venenatis, massa vel blandit pretium, justo ante rutrum mi, sit amet lacinia.', color: 'Orange', img: 'https://www.catster.com/wp-content/uploads/2018/01/Orange-tabby-cat-sleeping-with-eyes-closed.jpg'},
+    {id: 'pet-card1', type: 'Dino', name: 'Feathers', specialSkill: 'He does a great job keeping bad neighbors away. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacus lorem, condimentum fringilla posuere sed, commodo nec purus. Vestibulum felis nibh, lobortis sed erat.', color: 'Brown', img: 'http://www.scified.com/u/T-Rex_33.png'},
+    {id: 'pet-card2', type: 'Dog', name: 'Oceane', specialSkill: 'She makes great coffee. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id elementum purus. Mauris felis leo, imperdiet eu lorem consequat, interdum semper nibh. Pellentesque felis tortor, interdum sed nunc.', color: 'Black', img: 'https://i.ytimg.com/vi/ulYd7bMRTu4/maxresdefault.jpg'},
+    {id: 'pet-card3', type: 'Cat', name: 'Mona', specialSkill: 'He hides like an expert. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel tincidunt neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac nisl ut ante luctus cursus quis eget risus erut.', color: 'Black', img: 'https://images.unsplash.com/photo-1570018144466-5a3251ecd588?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'},
+    {id: "pet-card4", type: 'Dog', name: 'Koro', specialSkill:'He protects like a champ. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat sem justo, vel fermentum nisl faucibus et. Vestibulum venenatis orci purus, efficitur molestie libero pulvinar at. Morbi molleras.', color: 'Gray', img: 'https://i.pinimg.com/originals/28/8b/52/288b527eb15362865e5e8f3bb9a6862b.jpg'}
 ];
 
 const printToDom = (divId, textToPrint) => {
@@ -25,77 +13,18 @@ const printToDom = (divId, textToPrint) => {
 
 const petCards = () => {
     let domString = '';
-    for (let i = 0; i < pet.length; i++) {
-        domString += '<div class="adoptee">';
-        domString += `<header>${pet[i].name}</header>`;
-        domString +=    `<img src=${pet[i].img}></img>`;
-        domString +=    `<section class="colors">${pet[i].color}</section>`;
-        domString +=    `<p class="skills">${pet[i].specialSkill}</p>`;
-        domString +=    `<footer class="Cat">${pet[i].type}</footer>`;
+    for (let i = 0; i < pets.length; i++) {
+        domString += `<div class="pet" id=${pets[i].id}>`;
+        domString += `<header>${pets[i].name}</header>`;
+        domString +=    `<img src=${pets[i].img}></img>`;
+        domString +=    `<section class="colors">${pets[i].color}</section>`;
+        domString +=    `<p class="skills">${pets[i].specialSkill}</p>`;
+        domString +=    `<footer class=${pets[i].type}>${pets[i].type}</footer>`;
         domString += '</div>';
     }
     printToDom('pet-cards', domString);
 };
 
-const petCards2 = () => {
-    let domString2 = '';
-    for (let i = 0; i < pet2.length; i++) {
-        domString2 += '<div class="adoptee1">';
-        domString2 += `<header>${pet2[i].name}</header>`;
-        domString2 +=    `<img src=${pet2[i].img}></img>`;
-        domString2 +=    `<section class="colors">${pet2[i].color}</section>`;
-        domString2 +=    `<p class="skills">${pet2[i].specialSkill}</p>`;
-        domString2 +=    `<footer class="Dino">${pet2[i].type}</footer>`;
-        domString2 += '</div>';
-    }
-    printToDom('pet-cards2', domString2);
-};
-
-const petCards3 = () => {
-    let domString3 = '';
-    for (let i = 0; i < pet3.length; i++) {
-        domString3 += '<div class="adoptee2">';
-        domString3 += `<header>${pet3[i].name}</header>`;
-        domString3 +=    `<img src=${pet3[i].img}></img>`;
-        domString3 +=    `<section class="colors">${pet3[i].color}</section>`;
-        domString3 +=    `<p class="skills">${pet3[i].specialSkill}</p>`;
-        domString3 +=    `<footer class="Dog">${pet3[i].type}</footer>`;
-        domString3 += '</div>';
-    }
-    printToDom('pet-cards3', domString3);
-};
-
-const petCards4 = () => {
-    let domString4 = '';
-    for (let i = 0; i < pet4.length; i++) {
-        domString4 += '<div class="adoptee3">';
-        domString4 += `<header>${pet4[i].name}</header>`;
-        domString4 +=    `<img src=${pet4[i].img}></img>`;
-        domString4 +=    `<section class="colors">${pet4[i].color}</section>`;
-        domString4 +=    `<p class="skills">${pet4[i].specialSkill}</p>`;
-        domString4 +=    `<footer class="Cat">${pet4[i].type}</footer>`;
-        domString4 += '</div>';
-    }
-    printToDom('pet-cards4', domString4);
-};
-
-const petCards5 = () => {
-    let domString5 = '';
-    for (let i = 0; i < pet5.length; i++) {
-        domString5 += '<div class="adoptee1">';
-        domString5 += `<header>${pet5[i].name}</header>`;
-        domString5 +=    `<img src=${pet5[i].img}></img>`;
-        domString5 +=    `<section class="colors">${pet5[i].color}</section>`;
-        domString5 +=    `<p class="skills">${pet5[i].specialSkill}</p>`;
-        domString5 +=    `<footer class="Dog">${pet5[i].type}</footer>`;
-        domString5 += '</div>';
-    }
-    printToDom('pet-cards5', domString5);
-};
-
 petCards();
-petCards2();
-petCards3();
-petCards4();
-petCards5();
+
 
