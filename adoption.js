b8e7da89ff62e40e290a7b98a74d2ac902c5e5d5
Will Baker <1will.baker@gmail.com>
2020-01-28 17:02:45 -0600

    streamlined the button functions and added new pets into the array

--- adoption.js
+++ adoption.js
@@ -3,7 +3,11 @@ const pets = [
     {id: 'pet-card1', type: 'Dino', name: 'Feathers', specialSkill: 'He does a great job keeping bad neighbors away. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacus lorem, condimentum fringilla posuere sed, commodo nec purus. Vestibulum felis nibh, lobortis sed erat.', color: 'Brown', img: 'http://www.scified.com/u/T-Rex_33.png'},
     {id: 'pet-card2', type: 'Dog', name: 'Oceane', specialSkill: 'She makes great coffee. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id elementum purus. Mauris felis leo, imperdiet eu lorem consequat, interdum semper nibh. Pellentesque felis tortor, interdum sed nunc.', color: 'Black', img: 'https://i.ytimg.com/vi/ulYd7bMRTu4/maxresdefault.jpg'},
     {id: 'pet-card3', type: 'Cat', name: 'Mona', specialSkill: 'He hides like an expert. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel tincidunt neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac nisl ut ante luctus cursus quis eget risus erut.', color: 'Black', img: 'https://images.unsplash.com/photo-1570018144466-5a3251ecd588?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'},
-    {id: "pet-card4", type: 'Dog', name: 'Koro', specialSkill:'He protects like a champ. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat sem justo, vel fermentum nisl faucibus et. Vestibulum venenatis orci purus, efficitur molestie libero pulvinar at. Morbi molleras.', color: 'Gray', img: 'https://i.pinimg.com/originals/28/8b/52/288b527eb15362865e5e8f3bb9a6862b.jpg'}
+    {id: "pet-card4", type: 'Dog', name: 'Koro', specialSkill:'He protects like a champ. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat sem justo, vel fermentum nisl faucibus et. Vestibulum venenatis orci purus, efficitur molestie libero pulvinar at. Morbi molleras.', color: 'Gray', img: 'https://i.pinimg.com/originals/28/8b/52/288b527eb15362865e5e8f3bb9a6862b.jpg'},
+    {id: "pet-card5", type: 'Dino', name: 'Tommy', specialSkill:'Can carry you a very long way. faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit duis. euismod elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus. Mori esperanza.', color: 'Gray', img: 'https://www.wildrepublic.com/wp-content/uploads/2018/07/Brachiosaurus-3025-xl-600x404.jpg'},
+    {id: "pet-card6", type: 'Dog', name: 'Felixious', specialSkill: 'A loyal companion to the very end. quisque non tellus orci ac auctor augue mauris augue neque gravida in fermentum et sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt. Loborium espa.', color: 'Brown', img: 'https://www.keystonepuppies.com/wp-content/uploads/2018/10/Chocolate-Lab-Category.jpg'},
+    {id: "pet-card7", type: 'Cat', name: 'Adam', specialSkill: 'Seems delusional about thinking that it is a person.nec dui nunc mattis enim ut tellus elementum sagittis vitae et leo duis ut diam quam nulla porttitor massa id. morbi tincidunt augue interdum velit ipsum faucibus vitae al.', color: 'White', img: 'http://www.petsworld.in/blog/wp-content/uploads/2014/09/Ragdoll1.jpg'},
+    {id: "pet-card8", type: 'Dino', name: 'Gumshoe', specialSkill: 'Always seems to stick to the bottom of your shoe like gum. vitae congue eu consequat ac felis donec et odio pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec.', color: 'Green', img: 'https://p7.hiclipart.com/preview/452/270/479/sheriff-woody-dino-buzz-lightyear-jessie-rex-green-dinosaurs.jpg'}
 ];
 
 const printToDom = (divId, textToPrint) => {
@@ -25,49 +29,34 @@ const petCards = (petsArray) => {
     printToDom('pet-cards', domString);
 };
 
-petCards(pets);
 
-const doggos = () => {
-    const petties = []
+
+
+const allPets = (e) => {
+    const buttonId = e.target.id;
+    if(buttonId === 'All'){
+        petCards(pets);
+    } else{
+    const daPets = []
         for (let i = 0; i < pets.length; i++){
-            if( pets[i].type === 'Dog'){
-                petties.push(pets[i]);
+            if( pets[i].type === buttonId){
+                daPets.push(pets[i]);
             }
         }
-    petCards(petties);
+    petCards(daPets);
+    }
 };
 
-const kitties = () => {
-    const petties = []
-        for (let i = 0; i < pets.length; i++){
-            if( pets[i].type === 'Cat'){
-                petties.push(pets[i]);
-            }
-        }
-    petCards(petties);
+
+const event = () => {
+    document.getElementById('Dog').addEventListener('click', allPets);
+    document.getElementById('Cat').addEventListener('click', allPets);
+    document.getElementById('Dino').addEventListener('click', allPets);
+    document.getElementById('All').addEventListener('click', allPets);
+}
+const init = () => {
+    petCards(pets);
+    event();
 };
 
-const dinos = () => {
-    const petties = []
-        for (let i = 0; i < pets.length; i++){
-            if( pets[i].type === 'Dino'){
-                petties.push(pets[i]);
-            }
-        }
-    petCards(petties);
-}
-
-const all = () => {
-    const petties = []
-        for (let i = 0; i < pets.length; i++){
-            petties.push(pets[i]);
-             }
-    petCards(petties);
-}
-
-
-document.getElementById('Dogs').addEventListener('click', doggos);
-document.getElementById('Cats').addEventListener('click', kitties);
-document.getElementById('Dino').addEventListener('click', dinos);
-document.getElementById('All').addEventListener('click', all);
-
+init();
